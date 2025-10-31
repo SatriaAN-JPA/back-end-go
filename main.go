55b700cd0921a4712b9ef0e234aa946ac3bd7504
SatriaAN-JPA <satria.ardhya@jatelindo.co.id>
2025-10-31 14:24:12 +0700

    Update: Front-end URL latest

--- main.go
+++ main.go
@@ -2,31 +2,49 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-  r := gin.Default()
+	_ = godotenv.Load()
 
-  r.Use(cors.New(cors.Config{
-    AllowOrigins:     []string{"http://localhost:3000"},
-    AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-    AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-    ExposeHeaders:    []string{"Content-Length"},
-    AllowCredentials: true,
-    MaxAge: 12 * time.Hour,
-  }))
+	r := gin.Default()
 
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"message": "PING!!!"})
-  })
+	// front := os.Getenv("FRONTEND_URL")
+	// if front == "" {
+	// 	front = "http://localhost:3000"
+	// }
+	front := "https://front-end-next-js-v1-gj6b2uz5m-satriaan-jpas-projects.vercel.app/"
 
-  r.GET("/apps", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"message": "Welcome To Task Manager"})
-  })
+	origins := strings.Split(front, ",")
 
-  r.Run(":8080")
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     origins,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "PING!!!"})
+	})
+
+	r.GET("/apps", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome To Task Manager"})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run(":" + port)
 }
